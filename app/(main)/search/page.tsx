613ae3015cb09b12663a60465e4d75b6909adb12
abdullohqurbon0v <abdullohqurbonov332@gmail.com>
2025-03-12 19:23:00 +0500

    fix param

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -1,48 +1,10 @@
-"use client";
-import { $axios } from "@/http/api";
-import { BlogTypes, ProductsType } from "@/types";
-import { useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import Product from "../components/product";
-import Accordions from "../components/accordions";
-import Blogs from "../components/blogs";
-import Footer from "../components/footer";
+import React, { Suspense } from "react";
+import SearchPage from "./SearchPage";
 
-const SearchPage = () => {
-  const [products, setProducts] = useState<ProductsType[]>([]);
-  const [blogs, setBlogs] = useState<BlogTypes[]>([]);
-  const param = useSearchParams();
-  const query = param.get("query");
-
-  useEffect(() => {
-    const getSearchParams = async () => {
-      const res = await $axios.get(`/search?query=${query}`);
-      setProducts(res.data);
-
-      const blogs = await $axios.get("/news");
-      setBlogs(blogs.data);
-      console.log(res);
-    };
-    getSearchParams();
-  }, [query]);
-
+export default function Page() {
   return (
-    <div className="min-h-screen mt-16">
-      <div className="max-w-[1200px] mx-auto pt-6">
-        <h1 className="my-16 text-3xl font-semibold">Продукты</h1>
-        <div className="grid grid-cols-3 gap-10">
-          {products.map((item) => (
-            <Product item={item} key={item.description_ru} />
-          ))}
-        </div>
-      </div>
-      <div className="my-16">
-        <Accordions />
-      </div>
-      <Blogs blogs={blogs} />
-      <Footer />
-    </div>
+    <Suspense fallback={<div className="text-white p-10">Загрузка...</div>}>
+      <SearchPage />
+    </Suspense>
   );
-};
-
-export default SearchPage;
+}
