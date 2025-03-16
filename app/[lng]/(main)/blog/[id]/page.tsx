8c1906c079b691274065d9568bde76f72c34fb30
abdullohqurbon0v <abdullohqurbonov332@gmail.com>
2025-03-16 20:55:25 +0500

    fix language change

--- app/[lng]/(main)/blog/[id]/page.tsx
+++ app/[lng]/(main)/blog/[id]/page.tsx
@@ -10,12 +10,13 @@ import Loading from "../../components/loading";
 const SingleBlog = () => {
   const [blog, setBlog] = useState<BlogTypes | null>(null);
   const [loading, setLoading] = useState(true);
-  const { id } = useParams() as { id: string };
+  const { id, lng } = useParams() as { id: string; lng: string };
 
   useEffect(() => {
     const fetchBlog = async () => {
       try {
         const res = await $axios.get(`/news/${id}`);
+        console.log(res);
         setBlog(res.data);
       } catch (error) {
         console.error("Ошибка при загрузке блога", error);
@@ -36,13 +37,16 @@ const SingleBlog = () => {
   }
 
   if (!blog) {
-    return <Loading/>
+    return <Loading />;
   }
 
   return (
     <div className="min-h-screen mt-16 bg-[#0F0F0F] text-white py-20 px-4">
       <div className="max-w-[800px] mx-auto">
-        <h1 className="text-4xl font-bold mb-4">{blog.title_ru}</h1>
+        <h1 className="text-4xl font-bold mb-4">
+          {" "}
+          {lng === "uz" ? blog.title_uz : blog.title_ru}
+        </h1>
         <p className="text-gray-400 text-sm mb-6">
           {new Date(blog.created_time).toLocaleDateString("ru-RU", {
             day: "numeric",
@@ -57,7 +61,9 @@ const SingleBlog = () => {
           height={400}
           className="rounded-xl mb-8 object-cover w-full h-[400px]"
         />
-        <p className="text-lg leading-7 text-gray-200">{blog.description_ru}</p>
+        <p className="text-lg leading-7 text-gray-200">
+          {lng === "uz" ? blog.description_uz : blog.description_ru}
+        </p>
       </div>
     </div>
   );
